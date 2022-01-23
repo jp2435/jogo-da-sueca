96627356d838e2a51630d8216c6b73d484d461e5
Jorge CP <64277389+jp2435@users.noreply.github.com>
2022-01-23 23:32:48 +0000

    Output dos SVG das cartas

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 import {baralho as BaralhoCompleto} from "./baralho.js"
 import { user } from "./user.js"
 
-// Criação de usuários e separação dos seus baralhos
+// Criação de usuários
 let User1 = new user(1)
 let User2 = new user(2)
 let User3 = new user(3)
 let User4 = new user(4)
 
-// Criação da variável para realizar as alterações
+const users = [
+    User1,
+    User2,
+    User3,
+    User4
+]
+
+// Criação da variável do baralho para realizar as alterações
 let BaralhoRounda = []
 
 BaralhoCompleto.forEach(value => {
@@ -38,9 +45,48 @@ function distruibuicao(array){
     }
 }
 
+function addSrc(user){
+    const {baralho} = user
+    baralho.forEach(value => {
+        value.src= value.nome=='Ás' ? `/img/${value.naipe.nome}_As.svg` :`/img/${value.naipe.nome}_${value.nome}.svg`
+    })
+}
+
+function clickOnCard(event){
+    const img = document.getElementById(event.path[0].id)
+    img.classList.toggle('hide-img')
+}
+
+// Para criação da div onde vai ser representado as cartas
+function CreateUserDiv(user){
+    let divUser = document.createElement('div')
+    let UserH4 = document.createElement('h4')
+    UserH4.innerHTML=`Jogador ${user.id}`
+    divUser.appendChild(UserH4)
+    let divCartas = document.createElement('div')
+    divCartas.setAttribute('class', 'dclasscartas')
+    const {baralho} = user
+
+    for(let i=0;i<baralho.length;i++){
+        let img = document.createElement('img')
+        img.src=baralho[i].src
+        img.setAttribute('id', `${user.id}-${i}`)
+        img.setAttribute('class', 'cartas-user')
+        img.addEventListener('click',clickOnCard)
+        divCartas.appendChild(img)
+        divUser.appendChild(divCartas)
+    }
+
+    divPrincipal.appendChild(divUser)
+}
+
 //Neste momento vai-se baralhar e distruibuir as cartas
 baralhamento(BaralhoRounda)
 distruibuicao(BaralhoRounda)
+
+users.forEach(value => {
+    addSrc(value)
+})
 
 // Informação sobre o naipe de trunfo da rounda
 const CartaTrunfo = User1.baralho[0]
@@ -48,9 +94,9 @@ const NaipeRounda = CartaTrunfo.naipe
 
 
 let CartaTrunfoH2 = document.createElement('h2')
-/*
-    * CartaTrunfo.naipe.nome == NaipeRounda.nome
-    * Return True
+/**
+* CartaTrunfo.naipe.nome == NaipeRounda.nome
+* @returns {boolean} True
 */
 CartaTrunfoH2.innerHTML = `Carta de trunfo: ${CartaTrunfo.nome} de ${CartaTrunfo.naipe.nome}`
 
@@ -59,33 +105,6 @@ NaipeH2.innerHTML=`Trunfo: ${NaipeRounda.nome}`
 
 let divPrincipal = document.createElement('div')
 divPrincipal.setAttribute('id','principal')
-
-function clickOnLi(event){
-    const li = document.getElementById(event.path[0].id)
-    li.classList.toggle('hide-li')
-}
-
-// Para criação da div onde vai ser representado as cartas
-const CreateUserDiv = (user) => {
-    let divUser = document.createElement('div')
-    let UserH4 = document.createElement('h4')
-    UserH4.innerHTML=`Jogador ${user.id}`
-    divUser.appendChild(UserH4)
-    let ulDiv = document.createElement('ul')
-    const {baralho} = user
-
-    for(let i=0;i<baralho.length;i++){
-        let li = document.createElement('li')
-        li.innerHTML=`${baralho[i].nome} de ${baralho[i].naipe.nome}`
-        li.setAttribute('id', `${user.id}-${i}`)
-        li.setAttribute('class', 'view-li')
-        li.addEventListener('click', clickOnLi)
-        ulDiv.appendChild(li)
-    }
-
-    divUser.appendChild(ulDiv)
-    divPrincipal.appendChild(divUser)
-}
 
 CreateUserDiv(User1)
 CreateUserDiv(User2)
